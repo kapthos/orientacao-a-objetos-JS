f06561b5ea7df4ba5a1e707c368b8f202202953d
kapthos <81396249+kapthos@users.noreply.github.com>
2021-10-15 05:13:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ class ContaCorrente { //criando classe ContaCorrente (molde do objeto)
     agencia;
     conta;
     saldo;
-    sacar(valor){                           //Nome do método e entre parenteses será o valor necessário para fazer a conta
+    sacar(valor){                           //Nome do método e entre parenteses será o valor necessário para fazer a operação
         if (correnteJuan.saldo >= valor){
             correnteJuan.saldo -= valor
+        }
+    }
+    depositar(valor){                       //Criando método depositar dentro da classe para ser usada nas instancias.
+        if (valor > 0){
+            correnteJuan.saldo += valor;
         }
     }
 }
@@ -22,8 +27,11 @@ correnteJuan.conta = 7894561;
 correnteJuan.saldo = 0;
 
 correnteJuan.saldo = 100;       // adicionando saldo à conta
-correnteJuan.sacar(100);        // chamando o método SACAR criado lá em cima e informando o VALOR especificado
+correnteJuan.sacar(50);        // chamando o método SACAR criado lá em cima e informando o VALOR especificado
+correnteJuan.depositar(300);
 
+
+//////////////
 
 const cliente2 = new Cliente();
 cliente2.nome = "Talita";
